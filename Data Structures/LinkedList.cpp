25d55bd442752ac7a98e00941546451aed994423
Subham-RKB <subhamchapagain45@gmail.com>
2021-07-03 14:29:58 +0545

    Set up

---
+++ Data Structures/LinkedList.cpp
@@ -0,0 +1,15 @@
+/*
+Singly linked list operations:
+1.appendNode()
+2.prpendNode()
+3.insertNodeAfter()
+4.deleteNodeByKey()
+5.updateNodeByKey()
+6.print()
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Oiiiii";
+}
