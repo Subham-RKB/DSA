2a9084e49e7cbc10d5cc672a286bdc3cb78db05a
Subham-RKB <subhamchapagain45@gmail.com>
2021-07-03 13:16:46 +0545

    two sum

---
+++ Questions/Twosum.c
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int check(vector<int>& nums,int target,int c){
+        for(int i=c+1;i<nums.size();i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v;
+        int a,b;
+        for(int i=0;i<nums.size();i++){
+            a=target-nums[i];
+            b=check(nums,a,i);
+            if(b!=-1){
+                v.push_back(i);
+                v.push_back(b);
+                break;
+            }
+        }
+        return v;
+    }
+};
