e43b00c47d136f5bdf9a5ebaa4d302a50b64495a
Subham-RKB <subhamchapagain45@gmail.com>
2021-08-06 14:33:05 +0545

    Permutation by recursive approach

--- Questions/next_permutation.cpp
+++ Questions/next_permutation.cpp
@@ -11,3 +11,25 @@ public:
         return ans;
     }
 };
+
+//Recursion Approach
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void permutation(vectr<int> &nums,int i){
+        if (i==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            permutation(nums,i+1);
+            swap(nums[i],nums[j]);
+        }
+
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        permutation(nums,0);
+        return ans;
+    }
+};
