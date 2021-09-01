ee7d052c1d96c1eb100b8474929b5a48a46305e9
Subham-RKB <subhamchapagain45@gmail.com>
2021-09-01 08:52:17 +0545

    Count the triplets

---
+++ Questions/GFG/#2_Array.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, int s)
+    {
+        // Your code here
+        int left=0;
+        int right=0;
+        int sum=arr[0];
+        while(left<n && right<n){
+            if(sum==s){
+                return {left+1,right+1};
+                
+            }
+            else if(sum>s){
+                sum=sum-arr[left];
+                left++;
+            }
+            else{
+                right++;
+                sum=sum+arr[right];
+                
+            }
+            
+        }
+        return {-1};
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        int arr[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
