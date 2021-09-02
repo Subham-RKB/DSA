52ec089aff757973469c96c57ebf6ba1503c8860
Subham-RKB <subhamchapagain45@gmail.com>
2021-09-02 16:14:36 +0545

    Finding Middle Element in a Linked List

---
+++ Questions/GFG/#1_LL.cpp
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+/* Function to get the middle of the linked list*/
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+/* Function to get the middle of the linked list*/
+int getMiddle(Node *head);
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		cout<<getMiddle(head)<<endl;
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+    if(head==NULL) return -1;
+    Node *temp1=head;
+    Node *temp2=head;
+    while(temp2!=NULL && temp2->next!=NULL){
+        temp1=temp1->next;
+        temp2=temp2->next->next;
+    }
+    return temp1->data;
+   // Your code here
+}
