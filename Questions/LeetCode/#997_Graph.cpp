cb4ffc0cfca6411eaffd1bf63dfefe99ed6ee017
Subham-RKB <subhamchapagain45@gmail.com>
2021-09-06 10:58:04 +0545

    Find the Town Hudge

---
+++ Questions/LeetCode/#997_Graph.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(trust.size()==0 && n==1) return 1;
+        vector<int> out(n+1,0);
+        vector<int> in (n+1,0);
+        for(auto a:trust){
+            out[a[0]]++;
+            in[a[1]]++;
+        }
+        for(int i=0;i<=n;i++){
+            if(out[i]==0 && in[i]==n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
