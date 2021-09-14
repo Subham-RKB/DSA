8161cc8569e1759719660e1dc9342fe5ed265c5e
Subham-RKB <subhamchapagain45@gmail.com>
2021-09-14 20:56:08 +0545

    Rotate Image

---
+++ Questions/LeetCode/#48_Matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(), matrix.end());
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<'\n';
+        }
+        for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+    }
+};
