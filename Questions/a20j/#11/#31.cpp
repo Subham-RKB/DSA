a8fc4406ee4f915b095703d9e83b1dc06efdf2ac
Subham-RKB <subhamchapagain45@gmail.com>
2021-10-13 21:01:33 +0545

    Dima and Friends

---
+++ Questions/a20j/#11/#31.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n)        for(auto i=0; i<(n); i++)
+#define loop(i,a,b)     for(auto i=(a); i<(b); i++)
+#define rloop(i,a,b)    for(auto i=a-1; i>=(b); i--) 
+#define all(x)          x.begin(),x.end() 
+#define len(x)          ((int)x.size()) 
+#define sqr(x)          ((x)*(x))
+#define mod             1000000007
+#define pb              push_back
+#define xx              first
+#define yy              second
+#define pcount          __builtin_popcount
+
+typedef long long       ll; 
+typedef string          st; 
+typedef vector<int>     vi; 
+typedef vector<string>      vs; 
+typedef map<int,int>    mii; 
+typedef map<string,int>     msi; 
+typedef pair<int,int>   pii; 
+typedef vector<pii>     vpii; 
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll tot=0;
+    rep(i,n){
+        ll a;
+        cin>>a;
+        tot=tot+a;
+    }
+    ll o = n+2; 
+    ll ans=0;
+    for(ll i=1;i<6;i++){
+        if(((i+tot)-o)%(n+1)!=0){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int _=1;
+    // cin >> _;
+    while(_--){
+        solve();
+    }
+    return 0;
+}
