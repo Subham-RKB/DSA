07810af890eb8c2e026c29a5faa6e226ed894774
Subham-RKB <subhamchapagain45@gmail.com>
2021-11-18 10:26:27 +0545

    Find All Numbers Disappeared in an Array

--- Questions/LeetCode/#287_Array.cpp
+++ Questions/LeetCode/#287_Array.cpp
@@ -3,7 +3,7 @@ public:
     int findDuplicate(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
             if(nums[abs(nums[i])-1]<0){
-                cout<<"ok"<<endl;
+                //cout<<"ok"<<endl;
                 return abs(nums[i]);
             }
             else nums[abs(nums[i])-1]*=(-1);
