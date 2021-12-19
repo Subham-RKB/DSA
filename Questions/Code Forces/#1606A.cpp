3ae868519deb468e3656fa7f42b386af45719168
Subham-RKB <subhamchapagain45@gmail.com>
2021-12-19 16:46:16 +0545

    AB Balance

---
+++ Questions/Code Forces/#1606A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n)        for(auto i=0; i<(n); i++)
+#define loop(i,a,b)     for(auto i=(a); i<(b); i++)
+#define rloop(i,a,b)    for(auto i=a-1; i>=(b); i--) 
+#define all(x)          x.begin(),x.end() 
+#define len(x)          ((int)x.size()) 
+#define sqr(x)          ((x)*(x))
+#define mod             1000000007
+#define pb              push_back
+#define xx              first
+#define yy              second
+#define pcount          __builtin_popcount
+
+typedef long long       ll; 
+typedef string          st; 
+typedef vector<int>     vi; 
+typedef vector<string>      vs; 
+typedef map<int,int>    mii; 
+typedef map<string,int>     msi; 
+typedef pair<int,int>   pii; 
+typedef vector<pii>     vpii; 
+
+void solve(){
+    string s;
+    cin>>s;
+    if(s.length()==1) cout<<s<<endl;
+    else{
+        if(s[0]==s[s.length()-1]) cout<<s<<endl;
+        else cout<<s.substr(0,s.length()-1)<<s[0]<<endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int _;
+    cin >> _;
+    while(_--){
+        solve();
+    }
+    return 0;
+}
