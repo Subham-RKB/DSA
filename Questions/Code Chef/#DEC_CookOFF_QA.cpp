196de38f0a9c5e526b7af6bc47bc6275d3f630f9
Subham-RKB <subhamchapagain45@gmail.com>
2021-12-20 09:37:58 +0545

    Codechef DEC Cook Off 2021

---
+++ Questions/Code Chef/#DEC_CookOFF_QA.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n)        for(auto i=0; i<(n); i++)
+#define loop(i,a,b)     for(auto i=(a); i<(b); i++)
+#define rloop(i,a,b)    for(auto i=a-1; i>=(b); i--) 
+#define all(x)          x.begin(),x.end() 
+#define len(x)          ((int)x.size()) 
+#define sqr(x)          ((x)*(x))
+#define mod             1000000007
+#define pb              push_back
+#define xx              first
+#define yy              second
+#define pcount          __builtin_popcount
+
+typedef long long       ll; 
+typedef string          st; 
+typedef vector<int>     vi; 
+typedef vector<string>      vs; 
+typedef map<int,int>    mii; 
+typedef map<string,int>     msi; 
+typedef pair<int,int>   pii; 
+typedef vector<pii>     vpii; 
+int countDivisors(int n)
+{
+    int cnt = 0;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            // If divisors are equal,
+            // count only one
+            if (n / i == i)
+                cnt++;
+ 
+            else // Otherwise count both
+                cnt = cnt + 2;
+        }
+    }
+    return cnt;
+}
+void solve(){
+   ll n;
+   cin>>n;
+   
+   //cout<<countDivisors(n-1)<<endl;
+    vector<int> v;
+    for(ll i=0;i<n;i++){
+        int a;
+        cin>>a;
+    }
+    sort(v.begin(),v.end());
+    ll ans= v[n/2]-v[n/2-1];
+    cout<<ans<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int _;
+    cin >> _;
+    while(_--){
+        solve();
+    }
+    return 0;
+}
