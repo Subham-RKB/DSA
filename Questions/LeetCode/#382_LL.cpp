d38706615493e3c497e54b72aed1ad7cb6f53e69
Subham-RKB <subhamchapagain45@gmail.com>
2022-01-07 11:59:55 +0545

    Linked List Random Node

---
+++ Questions/LeetCode/#382_LL.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* head;
+    int tot=0;
+    Solution(ListNode* head) {
+        this->head=head;
+        ListNode *temp=head;
+        while(temp){
+            tot++;
+            temp=temp->next;
+        }
+    }
+    int getRandom() {
+        ListNode *temp=head;
+        int randNum = rand()%((tot-1) + 1) +1;
+        //cout<<randNum<<endl;
+        randNum--;
+        while(randNum--){
+            temp=temp->next;
+        }
+        return temp->val;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
