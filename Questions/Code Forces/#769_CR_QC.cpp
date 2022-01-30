766ca244cd393d9c0fb1bd84f11259f8663508f1
Subham-RKB <subhamchapagain45@gmail.com>
2022-01-30 23:47:12 +0545

    Codeforces Round #769 (Div.2)

---
+++ Questions/Code Forces/#769_CR_QC.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n)        for(auto i=0; i<(n); i++)
+#define loop(i,a,b)     for(auto i=(a); i<(b); i++)
+#define rloop(i,a,b)    for(auto i=a-1; i>=(b); i--) 
+#define all(x)          x.begin(),x.end() 
+#define len(x)          ((int)x.size()) 
+#define sqr(x)          ((x)*(x))
+#define mod             1000000007
+#define pb              push_back
+#define xx              first
+#define yy              second
+#define pcount          __builtin_popcount
+
+typedef long long       ll; 
+typedef string          st; 
+typedef vector<int>     vi; 
+typedef vector<string>      vs; 
+typedef map<int,int>    mii; 
+typedef map<string,int>     msi; 
+typedef pair<int,int>   pii; 
+typedef vector<pii>     vpii; 
+
+void solve(){
+    ll a,b;
+    cin>>a>>b;
+    ll o =b-a;
+    ll ans=0;
+    ll k=0;
+    if((a|b) == b) ans=1;
+    else{
+        while(b){
+        if(a%2==0 && b%2!=0){
+            ans+=(pow(2,k));
+            a+=pow(2,k);
+            if((a|b)==b) break;
+        }
+        else if(a%2!=0 && b%2==0){
+            ans+=(pow(2,k));
+            b+=pow(2,k);
+            if((a|b)==b) break;
+        }
+        if((a|b)==b) break;
+        k++;
+        a/=2;
+        b/=2;
+        }
+        ans++;;
+    }
+    cout<<min(o,ans)<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int _;
+    cin >> _;
+    while(_--){
+        solve();
+    }
+    return 0;
+}
