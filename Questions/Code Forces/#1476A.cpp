ac8c97547f19d184366f305d3e04baacb003af6b
Subham-RKB <subhamchapagain45@gmail.com>
2022-02-25 15:40:02 +0545

    K -divisible Sum

---
+++ Questions/Code Forces/#1476A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n)        for(auto i=0; i<(n); i++)
+#define loop(i,a,b)     for(auto i=(a); i<(b); i++)
+#define rloop(i,a,b)    for(auto i=a-1; i>=(b); i--) 
+#define all(x)          x.begin(),x.end() 
+#define len(x)          ((int)x.size()) 
+#define sqr(x)          ((x)*(x))
+#define mod             1000000007
+#define pb              push_back
+#define xx              first
+#define yy              second
+#define pcount          __builtin_popcount
+
+typedef long long       ll; 
+typedef string          st; 
+typedef vector<int>     vi; 
+typedef vector<string>      vs; 
+typedef map<int,int>    mii; 
+typedef map<string,int>     msi; 
+typedef pair<int,int>   pii; 
+typedef vector<pii>     vpii; 
+
+void solve(){
+    ll n,k;
+    cin>>n>>k;
+    if(k>=n){
+        if(k%n==0){
+            cout<<k/n<<endl;
+        }
+        else{
+            cout<<(k/n)+1<<endl;
+        }
+    }
+    else{
+        ll y = k*ceil(1.0*n/k);
+        if(y%n==0){
+            cout<<y/n<<endl;
+        }
+        else{
+            cout<<(y/n)+1<<endl;
+        }
+
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int _;
+    cin >> _;
+    while(_--){
+        solve();
+    }
+    return 0;
+}
