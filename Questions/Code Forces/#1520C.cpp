a99fe992a46f5cfb0c1438a0ddec764c96c660cb
Subham-RKB <subhamchapagain45@gmail.com>
2022-02-28 10:37:03 +0545

    Not Adjacent Matrix

---
+++ Questions/Code Forces/#1520C.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n)        for(auto i=0; i<(n); i++)
+#define loop(i,a,b)     for(auto i=(a); i<(b); i++)
+#define rloop(i,a,b)    for(auto i=a-1; i>=(b); i--) 
+#define all(x)          x.begin(),x.end() 
+#define len(x)          ((int)x.size()) 
+#define sqr(x)          ((x)*(x))
+#define mod             1000000007
+#define pb              push_back
+#define xx              first
+#define yy              second
+#define pcount          __builtin_popcount
+
+typedef long long       ll; 
+typedef string          st; 
+typedef vector<int>     vi; 
+typedef vector<string>      vs; 
+typedef map<int,int>    mii; 
+typedef map<string,int>     msi; 
+typedef pair<int,int>   pii; 
+typedef vector<pii>     vpii; 
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n==2)
+    {
+        cout<<-1<<endl;
+        return;
+    }
+    int k =n*n;
+    int i=1;
+    int l = 0;
+    while(i<=k)
+    {
+        cout<<i<<" ";
+        i+=2;
+        l++;
+        if(l==n){
+            l=0;
+            cout<<endl;
+        }
+    }
+    i=2;
+    while(i<=k)
+    {
+        cout<<i<<" ";
+        i+=2;
+        l++;
+        if(l==n){
+            l=0;
+            cout<<endl;
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int _;
+    cin >> _;
+    while(_--){
+        solve();
+    }
+    return 0;
+}
