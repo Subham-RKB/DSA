0b2e7602a54a95b39b7f22da5ec6a1bb33320557
Subham-RKB <subhamchapagain45@gmail.com>
2022-04-05 01:07:04 +0530

    Capacity To Ship Packages Within D Days

---
+++ Questions/LeetCode/#1011_BS.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool helper(vector<int>& v,int d,int m)
+    {
+        int a = 0;
+        int count = 1;
+        int  i = 0;
+        for(int i=0;i<v.size();i++)
+        {
+            if(a+v[i]>m)
+            {
+                a=0;
+                count++;
+            }
+            a+=v[i];
+        }
+        return count>d;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l = *max_element(weights.begin(),weights.end());
+        int h = accumulate(weights.begin(),weights.end(),0);
+        int ans = 0;
+        while(l<h)
+        {
+            int mid = (h+l)/2;
+            if(helper(weights,days,mid))
+            {
+                l = mid+1;
+            }
+            else{
+                h = mid;
+            }
+        }
+        return l;
+    }
+};
+// 1 2 3 1 1
+// 1  8 
